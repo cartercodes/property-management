c687ee47c556f5afec423b70a4aaeeefe336edd4
Mitchell Carter <carter.svo@gmail.com>
2018-10-19 15:33:41 -0400

    form title component.

---
+++ src/components/formTitle.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+export function FormTitle(props) {
+    const { className, text } = props;
+    return (
+        <h1 className={`${props.className} form-title`}>{text}</h1>
+    )
+}
