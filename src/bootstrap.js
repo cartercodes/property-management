4b04036698d2669db6ff75990b3edc4a1b937444
Mitchell Carter <carter.svo@gmail.com>
2018-11-12 17:31:51 -0500

    finished require auth and created browser history manually

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
+// import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 import Layout from './components/layout';
 
-import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
+import Signin from './components/auth/signin';
 
+import history from './history';
+import requireAuth from './components/requireAuth';
+
+class Dashboard extends Component {
+  render() {
+    return (
+        <div>
+        hey there
+    </div>
+    )
+  }
+}
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Layout>
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' component={Signin}/>
             <Route path='/signup' component={Signup}/>
+
+            <Route path='/dashboard' component={requireAuth(Dashboard)}/>
           </Layout>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
- 
+
+import history from '../history';
+
 export default function(ComposedComponent) {
     class Authentication extends Component {
         componentWillMount() {
             if(!this.props.authenticated) {
-                this.props.history.push('/')
+                history.push('/');
             }
         }
-        componentWillUpdate() {
-        
+        componentWillUpdate(nextProps) {
+            if(!nextProps.authenticated) {
+                history.push('/');
+            }
         }
         render() {
             return <ComposedComponent/>
         }
     }
-     function mapStateToProps(state) {
+
+    function mapStateToProps(state) {
         const { authenticated } = state.auth;
         return { authenticated } 
     }
