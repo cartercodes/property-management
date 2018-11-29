680c51cddfc40eb4b17359a11331a673df929b7c
Mitchell Carter <carter.svo@gmail.com>
2018-11-29 14:34:27 -0500

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -5,13 +5,21 @@ import NewsletterArchive from './newsletterArchive';
 import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
-    render() {
+    render() { 
+        const latest = {
+            _id: "115",
+            title:"Happy Birthday",
+            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            date: new Date(),
+            imageUrl: "http://via.placeholder.com/960x258"
+
+        }
         return (
             <div className='newsletter-grid'>
                 {/* add button */}
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                <NewsletterLatest/>
+                <NewsletterLatest {...latest}/>
             </div>
         )
     }
