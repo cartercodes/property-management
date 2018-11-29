e4bdb490b0b0fec83d259b1b96eae5e349f5a4aa
Mitchell Carter <carter.svo@gmail.com>
2018-11-29 15:07:42 -0500

    built a button compoent

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -8,7 +8,7 @@ class NewsletterGrid extends Component {
     render() { 
         const latest = {
             _id: "115",
-            title:"Happy Birthday",
+            title:"Happy Holidays Fam",
             body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             date: new Date(),
             imageUrl: "http://via.placeholder.com/960x258"
