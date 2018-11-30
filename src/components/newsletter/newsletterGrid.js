d24cef7e572d18db136a726f1d1f9ba551225bd3
Mitchell Carter <carter.svo@gmail.com>
2018-11-30 13:22:40 -0500

    created and placed add newsletter button on newsletter grid

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -3,20 +3,27 @@ import React, { Component } from 'react';
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
 import NewsletterLatest from './newsletterLatest';
+import Button from '../button';
 
 class NewsletterGrid extends Component {
-    render() { 
-        const latest = {
-            _id: "115",
-            title:"Happy Holidays Fam",
-            body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            date: new Date(),
-            imageUrl: "http://via.placeholder.com/960x258"
+    
+    handleAddNewsletter = () => {
+        this.props.history.push('/newsletter/new');
+    }
 
+    render() {
+
+        const latest = {
+            _id: '115',
+            title: 'Happy Holidays Fam',
+            body: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/960x258'
         }
+
         return (
             <div className='newsletter-grid'>
-                {/* add button */}
+                <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
                 <NewsletterLatest {...latest}/>
