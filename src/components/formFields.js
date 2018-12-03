cfea3111025c597f939a06b415b82e2e5134853d
Mitchell Carter <carter.svo@gmail.com>
2018-12-03 11:47:10 -0500

    customized newsletter new form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
 
+
 export class FormInput extends Component {
     render() {
         const { className, title, input, type, placeholder } = this.props;
         return (
             <div className={`${className} form-input`}>
                 <label className='form-input__title'>{title}</label>
-                <input 
+                <input
                     className='form-input__input'
                     type={type}
                     {...input}
-                    placeholder={placeholder}
+                    placeholder={placeholder}    
                 />
             </div>
         )
     }
 }
 
-export class FormButton extends Component {
+export class FormTextArea extends Component {
     render() {
         const { className, title, input, type, placeholder } = this.props;
         return (
-            <div className={`${className} form-button`}>
-                <button 
-                    className='form-button__button'
+            <div className={`${className} form-textarea`}>
+                <label className='form-textarea__title'>{title}</label>
+                <textarea
+                    className='form-textarea__input'
                     type={type}
                     {...input}
-                    placeholder={placeholder}
+                    placeholder={placeholder} 
+                   
+                >
+                
+                </textarea>
+            </div>
+        )
+    }
+}
+
+
+export class FormButton extends Component {
+    render() {
+        const { className, title, input, type } = this.props;
+        return (
+            <div className={`${className} form-button`}>
+                <button
+                    className='form-button__button'
+                    type={type}
+                    {...input} 
                 >
                 {title}
                 </button>
