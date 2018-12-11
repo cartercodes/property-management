1a6c737b27b5dc9ac7d4a8394e1676207c683fea
Mitchell Carter <carter.svo@gmail.com>
2018-12-11 12:45:34 -0500

    finally finished newsletter new form

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+
 import NewNewsletterForm from './newsletterNewForm';
 
 class NewNewsletter extends Component {
+
     onSubmit = (fields) => {
+
         // if(button == 'submit') {
         //   // save new newsletter on the backend. perform a post request here.
         //   console.log('trying to submit to backend.');
         // } 
-        // this.props.history.push('/dashboard')
-        console.log('trying to submit');
+        this.props.history.push('/dashboard');
     }
-     onCancel = () => {
-        // this.props.history.push('/dashboard')
-        console.log('trying to cancel');
+
+    onCancel = () => {
+        this.props.history.push('/dashboard')
     }
+
     render() {
         return (
             <div className='new-newsletter'>
-                 <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit={(event) => this.onSubmit(event)}/>
+                <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
 }
+
 export default NewNewsletter;
