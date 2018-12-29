6a642dc02f1e1def55a8a0aae91785561bcb040f
Mitchell Carter <carter.svo@gmail.com>
2018-12-29 15:42:37 -0500

    mapped over item data to requestsitem component

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -5,8 +5,10 @@ import Button from '../button';
 
 import AnimateHeight from 'react-animate-height';
 
+import { ROOT_URL } from '../../config';
+
 class RequestsItem extends Component {
-    
+
     constructor() {
         super()
 
@@ -17,45 +19,48 @@ class RequestsItem extends Component {
 
     toggleDropdown = () => {
         var element = document.getElementById('requests-item');
-        if(this.state.height == 0) {
+        if (this.state.height == 0) {
             element.classList.add('bg-F8');
-            this.setState({height: 'auto'})
+            this.setState({ height: 'auto' })
         } else {
             element.classList.remove('bg-F8');
-            this.setState({height: 0})
+            this.setState({ height: 0 })
         }
     }
 
     render() {
+        const { _id, title, body, date, imageUrl, status } = this.props;
+        const parsedDate = new Date(date);
         return (
             <div id='requests-item' className='requests-item'>
-                <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
+                <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle' />
                 <div className='requests-item__title'>
-                    <div className='requests-item__title__text'>Yo my door fell down</div>
-                    <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
+                    <div className='requests-item__title__text'>{title}</div>
+                    <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down' />
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Max - Unit 115
                 </div>
                 <div className='requests-item__date'>
-                    09/15/97
+                    {parsedDate.getMonth() + 1}
+                    /
+                    {parsedDate.getDate()}
+                    /
+                    {parsedDate.getFullYear() - 2000}
                 </div>
-                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status')}/>
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status')} />
                 <div className='requests-item__description'>
                     <AnimateHeight
                         duration={300}
                         height={this.state.height}
                     >
                         <div className='item-description'>
-                            <img 
+                            <img
                                 className='item-description__img'
-                                src='http://via.placeholder.com/160x94'                        
+                                src={`${ROOT_URL}/${imageUrl}`}
                             />
                             <p className='item-description__text'>
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                {body}
                             </p>
                         </div>
                     </AnimateHeight>
